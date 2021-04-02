b0a5bea40774103f803325247374b8ed55b4e012
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-04-02 13:07:29 +0300

    Update SearchBar.jsx

--- movies/src/components/SearchBar/SearchBar.jsx
+++ movies/src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function SearchBar({ searchCategories }) {
+export default function SearchBar({ searchCategories, changeCategory, changeSearchTerm }) {
+
+  const [searchTerm, SetSearchTerm] = useState(`batman`);
+  const [category, setCategory] = useState(`SearchMovie`);
+
 
   let options = []
 
@@ -8,17 +12,32 @@ export default function SearchBar({ searchCategories }) {
     options.push(searchCategories[key][1]);
   }
 
+  const categoryChangedHandler = (e) => {
+    setCategory(e);
+  }
+
+  const inputChangeHandler = async (e) => {
+    SetSearchTerm(e);
+  }
+
+  useEffect (()=>{
+    console.log(category);
+  },[category])
+
+  useEffect (()=>{
+    console.log(searchTerm);
+  },[searchTerm])
+
 
   return (
     <form className='search-form' onSubmit={(e) => e.preventDefault()}>
-      <h2>search</h2>
-      <input
-        type='text '
-        className='form-input' />
-      <select name="categories" id="categories" className="categories">
-        <option value="a">By key word</option>
-        {options.map((op,i) => <option key={i} value={op}>{op}</option>)}
-        </select>
+      <h2>Search</h2>
+      <input type='text' className='form-input' defaultValue={searchTerm}
+      onChange={(e)=>inputChangeHandler(e.target.value)}/>
+      <select name="categories" id="categories" className="categories"
+        onChange={(e) => categoryChangedHandler(e.target.value)}>
+        {options.map((op, i) => <option key={i} value={op}>{op}</option>)}
+      </select>
     </form>
   )
 }
