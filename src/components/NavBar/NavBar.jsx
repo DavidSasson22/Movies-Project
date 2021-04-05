46e02b6f19006e0a619dee3ccb1dcef12fc29a10
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-04-05 21:13:25 +0300

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ export default function NavBar() {
             <Link to = {`/movies`} className="menu"><span>SEARCH </span><i className="search icon"></i>
             </Link></li>
           <li className="menu"><span>FAVORITES </span><i className="heart icon"></i></li>
-          <li className="menu"><span>ANALIZE </span><i className="chart bar icon"></i></li>
+          <li className="menu"><span>ANALYZE </span><i className="chart bar icon"></i></li>
         </ul>
       </nav>
     </div>
