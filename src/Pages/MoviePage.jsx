b637d8fe713403b396c11d6f9c9e83b1abc5e4a2
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-04-05 19:13:20 +0300

    key changed

--- src/Pages/MoviePage.jsx
+++ src/Pages/MoviePage.jsx
@@ -5,10 +5,10 @@ import SingleMoviePage from '../components/SingleMoviePage/SingleMoviePage';
 //Set Api defult settings
 const imdbBasic = {
   baseUrl: `https://imdb-api.com/en/API/`,
-  key: `k_nvs3d5mt`,
+  // key: `k_nvs3d5mt`,
   // key: `k_2ck3tfdo`
   // key: `k_tjxj5k6p`
-  // key: `k_r6q4a65u`,
+  key: `k_r6q4a65u`,
 }
 
 //Set local storage
