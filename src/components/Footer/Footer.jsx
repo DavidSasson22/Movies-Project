bea17b4acc0389f83f03958408fd1f4215611c44
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-04-07 00:09:14 +0300

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,14 +7,16 @@ export default function Footer() {
       <div className="footerNav">
         <ul className="footer">
           <li className="menu">
-            <a target="_blank" href="https://www.linkedin.com/in/david-sasson-67229219b/"><i className="linkedin icon"></i></a>
+            <a target="" href="https://www.linkedin.com/in/david-sasson-67229219b/"><i className="linkedin icon"></i></a>
           </li>
           <li><h4>David Sasson</h4></li>
           <li className="menu">
-            <a target="_blank" href="https://github.com/DavidSasson22" ><i className="github icon"></i></a>
+            <a target="" href="https://github.com/DavidSasson22" ><i className="github icon"></i></a>
           </li>
         </ul>
       </div>
     </div>
   )
 }
+
+//
